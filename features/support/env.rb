83631f66cbc4157e00f098a9e093c639ce68e761
David W Millar <david.w.millar@gmail.com>
2016-11-22 15:44:05 -0500

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,7 +2,7 @@ require 'aruba/cucumber'
 
 Before do
   @dirs = ['build/install/mvncl/bin']
-  # I test frequently over celular networks...
+  # I test frequently over cellular networks...
   @aruba_timeout_seconds = 15
 end
 
