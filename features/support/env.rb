c168081f77e8c28f87ba948f09aa84b0da98a93d
David W Millar <dave@Davids-MBP.fios-router.home>
2017-04-20 22:56:42 -0400

    moar jokes

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,8 +1,13 @@
 require 'aruba/cucumber'
 
 Before do
+
   @dirs = ['build/install/mvncl/bin']
-  # I test frequently over cellular networks...
+
+  # Time and tide wait for no man.
+  # Neither does testing when you're on a train
+  # I frequently test over cellular networks...
   @aruba_timeout_seconds = 15
+
 end
 
